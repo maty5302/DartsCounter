8f33b7424982ce7a751e23c2b7c927cd4c4bc588
maty5302 <matej.prudil@outlook.cz>
2026-09-25 19:10:39 +0200

    Refactor: Update property names to follow PascalCase and improve method signatures

--- Domain/Tournament.cs
+++ Domain/Tournament.cs
@@ -1,78 +1,117 @@
-﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Domain.Models;
 
 namespace Domain
 {
 	public class Tournament
 	{
-		public List<Match> matches { get; set; }
-		public List<PlayerDto> players { get; set; }
-		public List<List<Match>> allmatches { get; set; }
-		private int round { get; set; }
+		public List<Match> Matches { get; set; } = new();
+		public List<PlayerDto> Players { get; set; } = new();
+		public List<List<Match>> AllMatches { get; set; } = new();
+		public int Round { get; private set; }
+
+		// Backward compatibility aliases
+		public List<Match> matches
+		{
+			get => Matches;
+			set => Matches = value;
+		}
+
+		public List<PlayerDto> players
+		{
+			get => Players;
+			set => Players = value;
+		}
+
+		public List<List<Match>> allmatches
+		{
+			get => AllMatches;
+			set => AllMatches = value;
+		}
+
+		public bool IsFinished => Matches.Count == 1 && Matches[0].WinnerId != 0;
+
+		public PlayerDto? Winner
+		{
+			get
+			{
+				if (!IsFinished) return null;
+				return Players.FirstOrDefault(p => p.Id == Matches[0].WinnerId);
+			}
+		}
 
 		public Tournament(List<PlayerDto> players)
 		{
-			this.players = players;
-			this.matches = new List<Match>();
-			this.allmatches = new List<List<Match>>();
-			this.generateMatches();
+			if (players == null || players.Count < 2)
+			{
+				throw new ArgumentException("Tournament requires at least 2 players.", nameof(players));
+			}
+
+			Players = new List<PlayerDto>(players);
+			Matches = new List<Match>();
+			AllMatches = new List<List<Match>>();
+			GenerateInitialMatches();
 		}
 
-		private void generateMatches()
+		private void GenerateInitialMatches()
 		{
-			int matchesPerRound = this.players.Count / 2;
-			if (round == 0)
-				round = 1;
+			Round = 1;
+			int matchesPerRound = Players.Count / 2;
 
 			for (int i = 0; i < matchesPerRound; i++)
 			{
-				this.matches.Add(new Match(Convert.ToInt32(this.players[i].Id), Convert.ToInt32(this.players[this.players.Count - 1 - i].Id), round));
+				Matches.Add(new Match(Convert.ToInt32(Players[i].Id), Convert.ToInt32(Players[Players.Count - 1 - i].Id), Round));
 			}
-			allmatches.Add(this.matches);
+
+			AllMatches.Add(Matches);
 		}
 
-		public Match? getNextMatch()
+		public Match? GetNextMatch()
 		{			
-			return this.matches.Find(x => x.WinnerId == 0);
+			return Matches.Find(x => x.WinnerId == 0);
 		}
-		//if all matches are played, generate next round (getNextMatch() returns null)
-        public void generateNextRound()
+
+		public Match? getNextMatch() => GetNextMatch();
+
+		public bool GenerateNextRound()
 		{
-			List<PlayerDto> winners = new List<PlayerDto>();
-			foreach (Match match in matches)
+			// Do not advance if the current round has unfinished matches
+			if (Matches.Any(m => m.WinnerId == 0))
 			{
-				if (match.WinnerId != 0)
-                {
-                    var player = players.Find(x => x.Id == match.WinnerId);
-                    if (player != null)
-                    {
-                        winners.Add(player);
-                    }
-				}
+				return false;
 			}
-			if (winners.Count == matches.Count && winners.Count>=2)
+
+			var winners = new List<PlayerDto>();
+			foreach (var match in Matches)
 			{
-
-				matches = new List<Match>();
-				int matchesPerRound = winners.Count / 2;
-				round++;
-
-				//musi to byt winners po sobe
-				for (int i = 0; i < matchesPerRound; i++)
+				var player = Players.Find(x => x.Id == match.WinnerId);
+				if (player != null)
 				{
-					matches.Add(new Match(Convert.ToInt32(winners[i*2].Id), Convert.ToInt32(winners[i*2+1].Id), round));
+					winners.Add(player);
 				}
-
-				allmatches.Add(matches);
-
 			}
-			else
+
+			// If only 1 winner remains, the tournament has concluded
+			if (winners.Count < 2)
 			{
-				matches = new List<Match>();
-				int matchesPerRound = winners.Count / 2;
-				round++;
-				allmatches.Add(matches);
+				return false;
 			}
+
+			Matches = new List<Match>();
+			Round++;
+			int matchesPerRound = winners.Count / 2;
+
+			for (int i = 0; i < matchesPerRound; i++)
+			{
+				Matches.Add(new Match(Convert.ToInt32(winners[i * 2].Id), Convert.ToInt32(winners[i * 2 + 1].Id), Round));
+			}
+
+			AllMatches.Add(Matches);
+			return true;
 		}
 
+		public void generateNextRound() => GenerateNextRound();
 	}
 }
